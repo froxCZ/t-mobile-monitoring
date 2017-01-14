a50ce6178c5c295d957b639cd161f58d910f74f7
Vojtech.Udrzal <udrzalv@gmail.com>
2017-01-14 18:04:42 +0100

    fetching from api

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ var express = require('express')
 var app = express()
 
 app.get('/api/', function (req, res) {
-    res.send('Hello Wxeorasdld!')
+  var a = {x:"a"};
+  res.send(a)
 })
 
 app.get('/yes/', function (req, res) {
-    res.send('Hello Wxeoxxxrasdld!')
+  res.send('Hello Wxeoxxxrasdld!')
 })
 
 app.listen(4000, function () {
-    console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 })
