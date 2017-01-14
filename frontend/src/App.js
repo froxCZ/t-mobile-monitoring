934f00150cef2cc1841bac43f934295595dcfccf
Vojtech.Udrzal <udrzalv@gmail.com>
2017-01-14 18:16:31 +0100

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 
 class App extends Component {
   fetchApi(){
@@ -12,18 +13,28 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a onClick={this.fetchApi}>fetch api</a>
-      </div>
-    );
+      <Router history={hashHistory}>
+        <Route path='/' component={Container}>
+          <IndexRoute component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='*' component={NotFound} />
+        </Route>
+      </Router>
+    )
   }
 }
-
+const Container = (props) => <div>
+  <Nav />
+  {props.children}
+</div>
+const Nav = () => (
+  <div>
+    <Link to='/'>Home</Link>&nbsp;
+    <Link to='/login'>Login</Link>
+  </div>
+)
+const Home = () => <h1>Hello from Home!</h1>
+const Login = () => <h1>Login</h1>
+const NotFound = () => (
+  <h1>404.. This page is not found!</h1>)
 export default App;
