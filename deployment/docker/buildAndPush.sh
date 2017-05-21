373312b20b688a98f0a306ccf87d1d4f11dc105a
Vojtech.Udrzal <udrzalv@gmail.com>
2017-05-21 18:18:20 +0200

    deployment

--- deployment/docker/buildAndPush.sh
+++
@@ -1,5 +0,0 @@
-cp /home/frox/school/thesis/nodejs/bigmon/backend . -r
-cp /home/frox/school/thesis/nodejs/bigmon/frontend . -r
-docker build -t mediation-monitoring .
-docker tag mediation-monitoring udrzalv/mediation-monitoring:latest
-docker push udrzalv/mediation-monitoring    
